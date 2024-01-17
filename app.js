3df8610b334d8f901705d9e7faf821050e673907
Bigod <bigodbiel@gmail.com>
2024-01-17 14:59:30 +0700

    working

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
+const path = require("path");
 
-app.get("/", (req, res) => res.send("your site"));
-app.listen(PORT, () => {
-  console.log("it is working");
+app.get("/index", (req, res) => {
+  res.sendFile(path.join(__dirname, "./index.html"));
+});
+
+app.get("/:id", (req, res) => res.send("your site"));
+
+app.listen(process.env.PORT, () => {
+  console.log(`it is working ${process.env.PORT}`);
 });
