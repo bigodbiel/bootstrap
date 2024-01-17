898dc41021cc8fad9ed5f87aba6f6d6f8b8006af
Bigod <bigodbiel@gmail.com>
2024-01-17 15:18:16 +0700

    working initial site with cards in boots

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ require("dotenv").config();
 const app = express();
 const path = require("path");
 
+app.use(express.static(path.join(__dirname, "/node_modules/bootstrap/dist")));
+app.use("/public", express.static(path.join(__dirname, "/public")));
+
 app.get("/index", (req, res) => {
   res.sendFile(path.join(__dirname, "./index.html"));
 });
